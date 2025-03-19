51ec9aad1c986b83f1d53ec977ebe6ca3dafa729
Axstr0n <sketa.jan@gmail.com>
2025-03-19 09:45:00 +0100

    Fixed batch files

--- batch_files/editor_preview.bat
+++ batch_files/editor_preview.bat
@@ -1,2 +1,8 @@
 @echo off
+cd /d %~dp0
+cd ..
+call venv\Scripts\activate
+cd src
 python -m preview.editor_preview
+cd ..
+deactivate
--- batch_files/navmesh_preview.bat
+++ batch_files/navmesh_preview.bat
@@ -1,2 +1,8 @@
 @echo off
+cd /d %~dp0
+cd ..
+call venv\Scripts\activate
+cd src
 python -m preview.navmesh_preview
+cd ..
+deactivate
